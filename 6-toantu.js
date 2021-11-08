c7e1c69a32a7b79c84d515289ad825398a548378
trungtvnd <tranviettrung20011992@gmail.com>
2021-11-08 13:27:31 +0700

    add toantu.js

---
+++ 6-toantu.js
@@ -0,0 +1,18 @@
+// dùng function
+// function hcn() {
+//     let weight = prompt('Nhap chiều rộng')
+//     let height = prompt('Nhập chiều dài')
+//     let area = weight * height
+//     document.write('Diện tích hình chữ nhật là' + area)
+// }
+
+// không dùng function
+let weight = prompt('Nhap chiều rộng')
+let height = prompt('Nhập chiều dài')
+let w = parseInt(weight)
+let h = parseInt(height)
+
+let area = w * h
+
+document.write('Diện tích HCN là: ' + area)
+
